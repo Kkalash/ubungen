5ea21aef67318843fdb6018e2863100f28844021
Khaldoun Kalash <40922644+Kkalash@users.noreply.github.com>
2020-07-07 12:23:29 +0200

    Update

---
+++ AllowInteger/AllowInteger/Form1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AllowInteger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Bitte geben Sie nur Zahlen ein!");
+            }
+            else
+            {
+                e.Handled = false;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                MessageBox.Show(String.Format("Sie haben {0} eingegeben.", textBox1.Text));
+            }
+        }
+
+
+    }
+}
