c83cbb950539f32cead63f912db8cd4a30e262a6
Khaldoun Kalash <40922644+Kkalash@users.noreply.github.com>
2020-07-07 15:43:28 +0200

    Update

---
+++ RandomOrder/RandomOrder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomOrder
+{
+    class Program
+    {
+        public static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            var sortedList = new List<int>() {1, 2, 3, 4, 5, 6 };
+            var randomList = RandomOrder(sortedList);
+            Console.WriteLine(String.Join(",", sortedList));
+            Console.WriteLine(String.Join(",", randomList));
+        }
+
+        private static IList<T> RandomOrder<T>(IList<T> sortedList)
+        {
+            //IList<T> randomList = sortedList;
+            //int n = sortedList.Count();
+            //while(n > 0)
+            //{
+            //    n--;
+            //    int randNum = rand.Next(n);
+            //    var value = sortedList[randNum];
+            //    sortedList[randNum] = sortedList[n];
+            //    sortedList[n] = value;
+            //}
+
+            var randomList = sortedList.OrderBy(a => rand.Next()).ToList();
+            return randomList;
+        }
+    }
+}
