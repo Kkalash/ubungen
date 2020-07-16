befbc8ff0f116fd5e119f9850c5c57de1edb2002
Khaldoun Kalash <40922644+Kkalash@users.noreply.github.com>
2020-07-16 12:48:59 +0200

    Update

---
+++ LottoSimulation/LottoSimulation/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LottoSimulation
+{
+    class Program
+    {
+        public static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Lotto Simulation***");
+            var listOfInputNum = InputNumbers();
+            var lottoNums = StartLotto(listOfInputNum);
+            Console.WriteLine("Lotto Zahlen: ");
+            foreach (var num in lottoNums)
+            {
+                Console.WriteLine(num);
+            }
+            Console.Read();
+        }
+
+        private static List<string> StartLotto(IEnumerable<int> inputList)
+        {
+            var lottoNums = new List<string>();
+            var randNums = new List<int>();
+            var count = 0;
+            while (lottoNums.Count() != inputList.Count())
+            {
+                var nextNum = rand.Next(1, 50);
+                if (inputList.Contains(nextNum) && !randNums.Contains(nextNum))
+                {
+                    lottoNums.Add(nextNum.ToString() + " (" + count + " Spielen)");
+                }
+                count++;
+            }
+            return lottoNums;
+        }
+
+        private static List<int> InputNumbers()
+        {
+            var listOfNum = new List<int>();
+            var count = 1;
+            Console.WriteLine("Geben Sie 6 Zahlen ein mit Leerzeichen zwischen die Zahlen(Zwischen 1-49)");
+            while (count <= 6)
+            {
+                Console.Write("{0}. ", count);
+                var input = Console.ReadLine();
+                int inputNum = Int32.Parse(input);
+
+                if (CheckNum(inputNum) && !listOfNum.Contains(inputNum))
+                {
+                    listOfNum.Add(inputNum);
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine("Die Zahl darf nicht zwei mal vorgommen!");
+                    Console.WriteLine("Die Zahl soll zwischen 1 und 49 sein!");
+                }
+            }
+
+            return listOfNum;
+        }
+
+        private static bool CheckNum(int num)
+        {
+            return num >= 1 && num <= 49;
+        }
+    }
+}
---
+++ SequenceOfNumbers/SequenceOfNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace SequenceOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "112";
+            var count = 1;
+            Console.WriteLine("{0}. Folge: {1}", count, text);
+            while (count <= 15)
+            {
+                text = NextRound(text);
+                Console.WriteLine("{0}. Folge: {1}", count, text);
+                count++;
+            }
+        }
+
+        private static string NextRound(string text)
+        {
+            int length;
+            var result = new StringBuilder();
+            while ((length = text.Length) > 0)
+            {
+                var letter = text[0];
+                var lengthNew = (text = text.TrimStart(letter)).Length;
+                result.Append((length - lengthNew).ToString() + letter);
+            }
+            return result.ToString();
+        }
+    }
+}
