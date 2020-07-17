f9231f2b466b60f3e10ab9bd6b5a6b62559bb40a
Khaldoun Kalash <40922644+Kkalash@users.noreply.github.com>
2020-07-17 10:05:28 +0200

    Centrechner

---
+++ Centrechner/Centrechner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Centrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie die Cent ein: ");
+            var tottalCent = Int32.Parse(Console.ReadLine());
+            var dollar = Dollar(tottalCent);
+            var cent = Cent(tottalCent);
+            Console.Write("Das ergibt {0} Dollar und {1} Cent", dollar, cent);
+            Console.Read();
+        }
+
+        public static int Dollar(int cent)
+        {
+            return cent / 100;
+        }
+
+        private static int Cent(int cent)
+        {
+            return cent % 100;
+        }
+    }
+}
